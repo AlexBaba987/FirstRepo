69b6fb60f757fa558b83c601901a2ac16de341d6
Shree <Shree@DESKTOP-PSCKED2>
2023-01-27 00:09:25 +0530

    Test class updated

--- Log4JProj01/src/com/nt/test/Test01.java
+++ Log4JProj01/src/com/nt/test/Test01.java
@@ -1,5 +1,11 @@
 package com.nt.test;
 
+import com.nt.beans.Utility;
+
 public class Test01 {
+	public static void main(String[] args) {
+		Utility u =new Utility();
+		System.out.println(u.getSysDate());
+	}
 
 }
